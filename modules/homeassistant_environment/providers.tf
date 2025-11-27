a9cd2288ffc02dc95f8bd6a34ba7db08dfd29222
Brandon <brandon@parksbra.com>
2025-11-26 18:28:32 -0800

    add kubectl provider

--- modules/homeassistant_environment/providers.tf
+++ modules/homeassistant_environment/providers.tf
@@ -4,6 +4,10 @@ terraform {
       source = "hashicorp/kubernetes"
       version = "~> 2.38.0"
     }
+    kubectl = {
+      source = "alekc/kubectl"
+      version = "~> 2.1.3"
+    }
     helm = {
       source = "hashicorp/helm"
       version = "~> 3.1.1"
