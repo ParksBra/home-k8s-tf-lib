8c86bfbd042a1591adab3c5ca88d95a097caacfb
Brandon <brandon@parksbra.com>
2025-11-26 19:28:43 -0800

    remove eof

--- modules/homeassistant/charts.tf
+++ modules/homeassistant/charts.tf
@@ -244,9 +244,7 @@ resource "helm_release" "application" {
       },
       {
         name  = "configuration.initScript"
-        value = <<-EOF
-${data.jinja_template.init_script.result}
-EOF
+        value = data.jinja_template.init_script.result
       }
     ],
     [
