53f9f750fef9d9bbfeaa6dc0562d14114745f220
Brandon <brandon@parksbra.com>
2025-11-26 19:04:37 -0800

    add time provider

--- modules/akri/versions.tf
+++ modules/akri/versions.tf
@@ -4,5 +4,11 @@ terraform {
       source = "hashicorp/helm"
       version = "~> 3"
     }
+    time = {
+      source = "hashicorp/time"
+      version = "~> 0"
+    }
   }
 }
+
+provider "time" {}
