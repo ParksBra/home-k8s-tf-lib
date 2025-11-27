ccb8e9d7b5e5c489e64ce3d9173f9f2a2ada46af
Brandon <brandon@parksbra.com>
2025-11-26 17:42:50 -0800

    remove field_selector

--- modules/homeassistant_environment/akri.tf
+++ modules/homeassistant_environment/akri.tf
@@ -4,12 +4,12 @@ module "akri" {
     kubernetes_namespace.namespace,
   ]
 
-  namespace                        = kubernetes_namespace.namespace.id
+  namespace                      = kubernetes_namespace.namespace.id
 
-  udev_discovery_enabled        = true
-  udev_instance_name            = "${kubernetes_namespace.namespace.id}-udev"
+  udev_discovery_enabled         = true
+  udev_instance_name             = "${kubernetes_namespace.namespace.id}-udev"
   udev_discovery_group_recursive = true
-  udev_discovery_rules_list = local.akri_udev_discovery_rules_list
+  udev_discovery_rules_list      = local.akri_udev_discovery_rules_list
 }
 
 data "kubernetes_resources" "akri_udev_instances" {
@@ -18,7 +18,6 @@ data "kubernetes_resources" "akri_udev_instances" {
   ]
   api_version    = "akri.sh/v0"
   kind           = "Instance"
-  field_selector = "spec.configurationName==${module.akri.udev_instance_name}"
   namespace      = module.akri.namespace_id
-  limit          = 1
+  limit          = 1 # Hopefully in any future there will only be one instance and udev configuration. TODO: improve this if needed.
 }
