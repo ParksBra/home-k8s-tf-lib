f6916883be14d19fe2f2b2d30f34f197e5d3f73a
Brandon <brandon@parksbra.com>
2025-12-01 19:12:19 -0800

    update version values

--- modules/akri/versions.tf
+++ modules/akri/versions.tf
@@ -2,11 +2,13 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "~> 3"
+      version = "~> 3.0"
     }
     time = {
       source = "hashicorp/time"
-      version = "~> 0"
+      version = "~> 0.0"
     }
   }
+
+  required_version = "~> 1.14"
 }
--- modules/tigera_operator/versions.tf
+++ modules/tigera_operator/versions.tf
@@ -2,7 +2,9 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "~> 3"
+      version = "~> 3.0"
     }
   }
+
+  required_version = "~> 1.14"
 }
