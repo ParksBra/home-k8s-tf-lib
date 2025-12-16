e991f6857932b8867580f300d568bf480d3ebd57
Brandon <brandon@parksbra.com>
2025-12-15 18:14:50 -0800

    create longhorn module

---
+++ modules/longhorn/locals.tf
@@ -0,0 +1,12 @@
+locals {
+  repository = "https://charts.longhorn.io"
+  chart_reference = "longhorn"
+
+  chart_install_name = var.chart_install_name != null ? var.chart_install_name : local.chart_reference
+
+  namespace = var.namespace != null ? var.namespace : local.chart_install_name
+
+  tls_secret_name = "${local.chart_install_name}-ingress-tls"
+
+  ingress_path = "/"
+}
---
+++ modules/longhorn/outputs.tf
@@ -0,0 +1,54 @@
+output "namespace" {
+  description = "The name of the namespace in which Longhorn is deployed."
+  value       = data.kubernetes_namespace.namespace.metadata[0].name
+}
+
+output "chart_id" {
+  description = "The Helm release ID for Home Assistant."
+  value       = helm_release.application.id
+}
+
+output "chart_name" {
+  description = "The Helm chart reference for Home Assistant."
+  value       = helm_release.application.name
+}
+
+output "chart_reference" {
+  description = "The Helm chart reference for Longhorn."
+  value       = local.chart_reference
+}
+
+output "service_address" {
+  description = "The address of the Longhorn service."
+  value       = "${helm_release.application.name}.${data.kubernetes_namespace.namespace.metadata[0].name}.svc"
+}
+
+output "service_port" {
+  description = "The port of the Longhorn service."
+  value       = var.service_port
+}
+
+output "ingress_enabled" {
+  description = "Whether ingress is enabled for the Longhorn service."
+  value       = var.ingress_enabled
+}
+
+output "ingress_address" {
+  description = "The ingress address of the Longhorn service."
+  value       = var.ingress_enabled ? var.ingress_host_address : ""
+}
+
+output "tls_enabled" {
+  description = "Whether TLS is enabled for the Longhorn ingress."
+  value       = var.ingress_tls_enabled
+}
+
+output "tls_secret_name" {
+  description = "The name of the TLS secret used for Longhorn ingress."
+  value       = local.tls_secret_name
+}
+
+output "storage_class_name" {
+  description = "The name of the storage class created by Longhorn."
+  value       = var.storage_class_name
+}
